03543f1adb6988b24306a3d0586742633e908379
mohsink13 <mohsink13@gmail.com>
2016-09-07 10:59:17 +0100

    cleanup

--- Email/Email.Test.ConsoleUI/Program.cs
+++ Email/Email.Test.ConsoleUI/Program.cs
@@ -1,11 +1,6 @@
 ﻿using Email.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.ServiceBus.Messaging;
-using System.Runtime.Serialization;
 using Email.QueueManager;
 
 namespace Email.Test.ConsoleUI
@@ -14,25 +9,8 @@ namespace Email.Test.ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ISendable service = new SendGridEmailService("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");
-
-            //service.Send(new EmailMessage
-            //{
-            //    From = "mohsin.khan@wolterskluwer.com",
-            //    To = new List<string> { "mohsin.khan@wolterskluwer.com" },
-            //    EmailContent = "Test content from test console UI",
-            //    ContentType = EmailContentType.TextHtml,
-            //    Subject = "Test Subject from test console UI"
-
-            //});
-
+            
             var qm = QueueFactory.GetQueue();
-
-            ////send message
-            //var connectionString = "Endpoint=sb://wkemailservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=8/Du+d6qMBX96ZF+jAg0XB/zGoN1CoEq5ss481xIp9Y=";
-            //var queueName = "emailmessagequeue";
-
-            //var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
 
             var email = new Email.DomainModel.EmailMessage
             {
@@ -44,11 +22,6 @@ namespace Email.Test.ConsoleUI
             };
 
             qm.Put(email);
-
-            ////var message = new BrokeredMessage(email, new DataContractSerializer(typeof(Email.DomainModel.EmailMessage)));// ("this is a string message...");
-            //var message = new BrokeredMessage(email, new DataContractSerializer(typeof(Email.DomainModel.EmailMessage)));// ("this is a string message...");
-            //client.Send(message);
-
 
             Console.Read();
         }
