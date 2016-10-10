25f1a773728f1b4b7632940ea31e352153d285d2
mohsink13 <mohsink13@gmail.com>
2016-10-10 14:50:32 +0100

    Added Email service client and hosted to Azure

---
+++ Email/Email.Web.Ui/Controllers/EmailController.cs
@@ -0,0 +1,42 @@
+﻿using ConfigurationManager;
+using Email.DomainModel;
+using Email.QueueManager;
+using Email.Web.Ui.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Email.Web.Ui.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Compose()
+        {
+            return View(new EmailMessageViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Compose(EmailMessageViewModel message)
+        {
+            var name = Config.ServiceBusQueueName;
+            using (var qm = QueueFactory.GetQueue<EmailMessage>())
+            {
+                var email = new EmailMessage
+                {
+                    From = message.From,
+                    To = new List<string> { message.To },
+                    EmailContent = message.EmailContent,
+                    ContentType = EmailContentType.TextHtml,
+                    Subject = message.Subject
+                };
+
+                qm.Enqueue(email);
+            }
+            TempData["message"] = "Message sent !!!";
+            return RedirectToRoute(new { action="compose", controller="email" });
+        }
+    }
+}
---
+++ Email/Email.Web.Ui/Models/EmailMessageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Email.Web.Ui.Models
+{
+    public class EmailMessageViewModel
+    {
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string To { get; set; }
+        public string EmailContent { get; set; }
+        public string ContentType { get; set; }
+
+
+    }
+}
